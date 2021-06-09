313fad6b859c3629f716caee4622e7b32821cf2a
keshav1222 <69798601+keshav1222@users.noreply.github.com>
2021-06-09 23:42:39 +0530

    added some more feature

--- crypto-currency-tracker/src/App.js
+++ crypto-currency-tracker/src/App.js
@@ -1,11 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Coin from './Coin';
+ 
+
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('')
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+      
+
+    }).catch(error => console.log(error))
+  }, []);
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+  const filteredCoins = coins.filter(coin =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="BI-FU-Crypto-Tracker"></header>
-      <h1>BI-FU-Crypto-Tracker</h1>
+    <div className='coin-app'>
+      <div className="coin-search">
+        <header className="BI-FU-Crypto-Tracker"></header>
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type = "text"  placeholder="Search" className = "coin-input" onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol} 
+          price ={coin.current_price}
+          volume={coin.market_cap} />
+        
+      })}
+
+
+      
+      
     </div>
   );
 }
